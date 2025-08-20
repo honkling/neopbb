f410af6543cd5a185962bc780dc4cc7ff391dd1e
Aquaotter <145584242+AquaotterDev@users.noreply.github.com>
2025-08-20 14:57:24 -0400

    removing period () since they aren't used just some cleanup.
    [small]

--- src/main/kotlin/me/honkling/neopbb/schedule/Period.kt
+++ src/main/kotlin/me/honkling/neopbb/schedule/Period.kt
@@ -6,13 +6,13 @@ enum class Period(
     vararg val flags: Flag,
     val isSpecial: Boolean = false
 ) {
-    RollCall(),
-    Breakfast(),
-    FreeTime(),
-    Lunch(),
-    JobTime(),
-    Dinner(),
-    CellTime(),
+    RollCall,
+    Breakfast,
+    FreeTime,
+    Lunch,
+    JobTime,
+    Dinner,
+    CellTime,
     LightsOut(Flag.CREATE_WORLD_FOG, Flag.DARKEN_SCREEN),
     Lockdown(Flag.CREATE_WORLD_FOG, Flag.DARKEN_SCREEN, isSpecial = true);
 }
